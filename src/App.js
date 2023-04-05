a8136e488ae2461ee64814f2977b5490d2bd69c6
Varsha <varshabhadana93@gmail.com>
2023-04-06 00:19:48 +0200

    Add show random animal feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+  const animals = ['dog', 'cat', 'cow', 'gator', 'bird', 'horse'];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+console.log(getRandomAnimal());
 
 function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+  const renderedAnimal = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div> {renderedAnimal} </div>
     </div>
   );
 }
